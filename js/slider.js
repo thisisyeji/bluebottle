a2777c04c4bd0a76cc6337a8a0b1eaad7a164d8a
yjkim0109 <yjkkim0109@gmail.com>
2022-07-24 12:24:48 +0900

    feat: add media screen

--- js/slider.js
+++ js/slider.js
@@ -1,10 +1,14 @@
 const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
-const slider = document.querySelector(".slider");
+const slider = document.querySelector(".img_slider");
 const ul = slider.querySelector("ul");
+let winWid = window.innerWidth;
+let left = parseFloat(getComputedStyle(ul).left);
+let per = (left / winWid) * 100;
 
-ul.style.left = "-100%"
-ul.prepend(ul.lastElementChild);
+ul.style.left = per + "%";
+
+console.log(per);
 
 
 next.addEventListener("click", (e) => {
@@ -20,11 +24,11 @@ prev.addEventListener("click", (e) => {
 function nextSlide() {
     new Anime(ul, {
         prop: "left",
-        value: "-200%",
+        value: per * 2 + "%",
         duration: 1000,
         callback: () => {
             ul.append(ul.firstElementChild);
-            ul.style.left = "-100%"
+            ul.style.left = per + "%"
         }
     })
 }
@@ -36,7 +40,7 @@ function prevSlide() {
         duration: 1000,
         callback: () => {
             ul.prepend(ul.lastElementChild);
-            ul.style.left = "-100%"
+            ul.style.left = per + "%"
         }
     })
 }
