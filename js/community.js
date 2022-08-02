619b7d707f394b4651d15c183bd406dd1ade27dc
yjkim0109 <yjkkim0109@gmail.com>
2022-08-03 01:26:06 +0900

    feat: responsive community page

---
+++ js/community.js
@@ -0,0 +1,59 @@
+// 함수
+const community = document.querySelector(".community");
+const btns = community.querySelectorAll(".btns li");
+const boxes = community.querySelectorAll("article>div");
+const title = community.querySelector("h2");
+
+btns.forEach((el, index) => {
+    el.addEventListener("click", e => {
+        e.preventDefault();
+        activation(btns, index);
+        activation(boxes, index);
+
+        title.innerText = e.target.innerText;
+    })
+})
+
+function activation(arr, index) {
+    for (let el of arr) {
+        el.classList.remove("on");
+    }
+    arr[index].classList.add("on");
+}
+
+
+
+/* class 아직 잘 모르겠음
+window.onload = () => {
+    new Tab(".community");
+}
+
+
+class Tab {
+    constructor(el) {
+        this.community = document.querySelector(el);
+        this.btns = this.community.querySelectorAll(".btns li");
+        this.boxes = this.community.querySelectorAll("article>div");
+        this.title = this.community.querySelector("h2");
+
+
+        this.btns.forEach((el, index) => {
+            el.addEventListener("click", e => {
+                e.preventDefault();
+
+                this.activation(this.btns, index);
+                this.activation(this.boxes, index);
+
+                this.title.innerText = e.target.innerText;
+            })
+        })
+
+        activation = function (arr, index) {
+            for (let el of arr) {
+                el.classList.remove("on");
+            }
+            arr[index].classList.add("on");
+        }
+    }
+}
+*/
